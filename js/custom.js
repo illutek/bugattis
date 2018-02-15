50fcc06fb57f9ebc1ae691b13ccb91f9bd4346ed
Vandenborne Stefan <stefan@illutek.be>
2018-02-15 20:24:10 +0100

    background images replace

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,11 @@
     $(document).ready(function () {
         var base = window.Drupal.settings.basePath + 'sites/default/files/background_images/';
         $.backstretch([
-            base + "background_15-min.jpg",
-            base + "background_16-min.jpg",
-            base + "background_17-min.jpg",
-            base + "background_18-min.jpg"
+            base + "background_20-min.jpg",
+            base + "background_21-min.jpg",
+            base + "background_22-min.jpg",
+            base + "background_23-min.jpg",
+            base + "background_24-min.jpg"
         ], {
             fade: 1050,
             duration: 7000
